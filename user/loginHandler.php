c181f53593e7b84c9c0d376b3fd8f79118b72d49
N-pacis <pacisnkubito@gmail.com>
2021-05-07 15:08:36 +0200

    Added updated database

--- user/loginHandler.php
+++ user/loginHandler.php
@@ -119,7 +119,7 @@
                        header("location:view_users.php");
                    }
                    else{
-                       echo $mac;
+                       echo mysqli_error($con);
                    }
                }
            }
